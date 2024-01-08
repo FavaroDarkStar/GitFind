c59096103f0aaa14db907aafa520046bfab737d9
FavaroDarkStar <favarosoftwareengineer@gmail.com>
2024-01-08 18:47:15 -0300

    implementada api do github

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,30 @@
+import { useState } from "react"
 import { Header } from "../../components/Header"
 import ItemList from "../../components/ItemList"
 import background from "../../assets/background.png"
 import './styles.css'
 
 function App() {
+
+  const [username, setUsername] = useState("");
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${username}`)
+    const newUser = await userData.json();
+    if(newUser.name){
+      const {avatar_url, name, bio, login} = newUser;
+      setCurrentUser({avatar_url, name, bio, login});
+
+      const reposData = await fetch(`https://api.github.com/users/${username}/repos`)
+      const newRepos = await reposData.json();
+      if(newRepos.length){
+        setRepos(newRepos);
+      }
+    }
+  };
+
   return (
     <div className="App">
       <Header />
@@ -12,24 +33,34 @@ function App() {
         <img src={background} className="background" alt="logo do github como imagem de background da tela" />
         <div className="info">
           <div>
-            <input name="user" placeholder="@username" />
-            <button>Search</button>
+            <input name="username" placeholder="@username" value={username} onChange={event => setUsername(event.target.value)} />
+            <button onClick={handleGetData}>Search</button>
           </div>
-          <div className="profile">
-            <img src="https://avatars.githubusercontent.com/u/106363360?v=4" className="profilePicture" alt="imagem de perfil do usuário no github"/>
-            <div>
-              <h3>Favaro</h3>
-              <span>@FavaroDarkStar</span>
-              <p> description </p>
+
+          {currentUser?.name ? (
+            <>
+            <div className="profile">
+              <img src={currentUser.avatar_url} className="profilePicture" alt="imagem de perfil do usuário no github"/>
+              <div>
+                <h3>{currentUser.name}</h3>
+                <span>@{currentUser.login}</span>
+                <p>{ currentUser.bio }</p>
+              </div>
             </div>
-          </div>
-          <hr />
-          <div>
-            <h4 className="repositories">Repositories</h4>
-            <ItemList tittle="titulo" description="descrição"/>
-            <ItemList tittle="titulo" description="descrição"/>
-            <ItemList tittle="titulo" description="descrição"/>
-          </div>
+            <hr />
+            </>
+          ) : console.log(null)}
+
+          {repos?.length ? (
+            
+            <div>
+                <h4 className="repositories">Repositories</h4>
+                {repos.map(repo => (
+                  <ItemList tittle={repo.name} description={repo.description}/>
+                ))}
+
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
